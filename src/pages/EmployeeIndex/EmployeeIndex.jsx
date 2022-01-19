ee671c3031129f98649081d9e594c17572b6f0f2
crice802 <rcrice@gmail.com>
2022-01-19 18:48:00 -0500

    show all complete

--- src/pages/EmployeeIndex/EmployeeIndex.jsx
+++ src/pages/EmployeeIndex/EmployeeIndex.jsx
@@ -3,18 +3,18 @@ import React, { useState } from 'react'
 import EmployeeCard from '../../components/Employee/EmployeeCard'
 
 function EmployeeIndex({employees, handleDeleteEmployee}) {
-  const [isActive, setIsActive] = useState(false)
+  const [isActive1, setIsActive1] = useState(false)
   
   return (
     
     <>
     <button
-     onClick={()=> setIsActive(!isActive)}
+     onClick={()=> setIsActive1(!isActive1)}
     >Show All</button>
     {employees.map(employee => 
       <EmployeeCard
-      // isActive={isActive}
-      // setIsActive={setIsActive}
+      isActive1={isActive1}
+      setIsActive1={setIsActive1}
       key={employee.id}
       employee={employee}
       handleDeleteEmployee={handleDeleteEmployee}
